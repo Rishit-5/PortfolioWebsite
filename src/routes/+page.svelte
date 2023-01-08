ab89258884bb9347adb5fd58d1da0bf4044367fc
Rishit-5 <rishistar05@gmail.com>
2023-01-07 20:25:42 -0600

    Center name div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     const layers = [0,1,2,3];
-
     let y;
+    function doAlert(){
+
+    }
 </script>
 
 <svelte:window bind:scrollY={y}/>
@@ -12,23 +14,27 @@
                 style="transform: translate(0, {-y * layer / (layers.length - 1)}px)"
                 src="src/assets/{layer}.png"
                 alt="parallax layer {layer}"
-        >
+        />
     {/each}
-]</div>
+</div>
 
 <div class="text">
 	<div class = "centerParallaxText">
-		Rishit Patil
+        <div class = "contents">
+            <h1 on:click = {doAlert()}>Rishit Patil</h1>
+            <p>Computer Science @ UW-Madison</p>
+        </div>
 	</div>
 
     <div class="foreground">
-        You have scrolled {y} pixels
+        <button on:click = {doAlert}>whns</button>
     </div>
 </div>
 
 <style>
     .parallax-container {
         position: fixed;
+        z-index: -1;
         width: 275vh;
         height: 500px;
         left: 50%;
@@ -37,6 +43,7 @@
     @media only screen and (max-device-width: 1366px) {
         .parallax-container {
             position: fixed;
+            z-index: -1;
             width: 350vh;
             height: 250px;
             left: 50%;
@@ -45,12 +52,14 @@
     @media only screen and (max-device-height: 800px) {
         .parallax-container {
             position: fixed;
+            z-index: -1;
             height: 100%;
             left: 50%;
             transform: translate(-50%,0%);        }
     }
     .parallax-container img {
         position: absolute;
+        z-index: -1;
         top: 0;
         left: 0;
         width: 100%;
@@ -67,13 +76,13 @@
 
     .text {
         position: relative;
+        z-index: 2;
         width: 100%;
         height: 300vh;
         font-size: 30px;
         text-align: center;
         justify-content: center;
         box-sizing: border-box;
-        pointer-events: none;
     }
 
     span {
@@ -88,21 +97,36 @@
         top: 900px;
         left: 0;
         width: 100%;
-        height: calc(100% - 712px);
+        height: 100%;
         background-color: rgb(32,0,1);
         color: white;
         padding: 50vh 0 0 0;
     }
     .centerParallaxText {
         position: relative;
-        left: 0;
         width: 100%;
-        height: calc(100% - 712px);
-        color: white;
-        padding: 50vh 0 0 0;
+        height: 25%;
+        color: aliceblue;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
-
-
+    .contents{
+        position: relative;
+        z-index: 1000;
+        height: auto;
+        width: auto;
+        padding: 5%;
+        /* From https://css.glass */
+        background: rgba(0, 0, 0, 0.44);
+        border-radius: 16px;
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(11.5px);
+        -webkit-backdrop-filter: blur(11.5px);
+        border: 1px solid rgba(0, 0, 0, 0.2);
+        font-family: 'Montserrat', sans-serif;
+        transform: translateY(25%);
+    }
 
     :global(body) {
         margin: 0;
