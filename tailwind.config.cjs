e1956a9a15b26a7dc8ea03bdb94e3878f0b49b23
Rishit-5 <rishistar05@gmail.com>
2023-01-15 02:08:38 -0600

    new svelte files

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+const config = {
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: { "50": "#fff1f2", "100": "#ffe4e6", "200": "#fecdd3", "300": "#fda4af", "400": "#fb7185", "500": "#f43f5e", "600": "#e11d48", "700": "#be123c", "800": "#9f1239", "900": "#881337" },
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+  darkMode: 'class',
+};
+
+module.exports = config;
