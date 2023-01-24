fa7cdef256d847dabd19d1b65e6f1b98f2ce8881
Rishit-5 <rishistar05@gmail.com>
2023-01-24 00:38:58 -0600

    projects section fully adapted to mobile

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -5,35 +5,52 @@
     let items = [];
     let activeItem = 'Teen Cafe';
     onMount(async() => {
-        let { data, error } = await supabase.from('projects').select('*')
+        let { data, error } = await supabase.from('projects').select('*').order('priority', { ascending: true })
         projects = data;
+        console.log(projects)
     })
 </script>
 {#each projects as project, index}
     {#if index % 2 === 0}
-    <!--<div class = "projectsDiv">-->
-    <!--    <p class = "projectText">{project.name}</p>-->
-    <!--    <div class="div1">{project.description}</div>-->
-    <!--    <img class="div2" src = {project.picture_url}/>-->
         <div class = "wrappedBox">
         <div class="wrapper">
-            <img class="two" src = {project.picture_url}/>
+            <a class="two" href = {project.url} target = "_blank">
+            <img class="two2" src = {project.picture_url}/>
+            <div class="centered">
+                <div class = "font-bold my-3">
+                    {project.name}
+                </div>
+                <div class = "centeredText">
+                    {project.description}
+                </div>
+            </div>
+            </a>
             <div class="one">
                 <div class="goBottom">{project.name}</div>
             </div>
-            <div class="three"><p class="projDesc">{project.description}</p></div>
+            <div class="three"><p class="projDesc">{project.description}</p>
+            <p class = "toolsText">{project.tools}</p></div>
             <div class="five"></div>
         </div>
         </div>
-    <!--</div>-->
         {:else}
         <div class = "wrappedBox">
             <div class="wrapperLeft">
-                <img class="twoLeft" src = {project.picture_url}/>
+                <a class="twoLeft" href = {project.url} target = "_blank">
+                <img  class="twoLeft2" src = {project.picture_url}/></a>
+                <a class="centered" href = {project.url} target = "_blank">
+                    <div class = "font-bold my-3">
+                        {project.name}
+                    </div>
+                    <div class = "centeredText">
+                        {project.description}
+                    </div>
+                </a>
                 <div class="oneLeft">
                     <div class="goBottomLeft">{project.name}</div>
                 </div>
-                <div class="threeLeft"><p class="projDescLeft">{project.description}</p></div>
+                <div class="threeLeft"><p class="projDescLeft">{project.description}</p>
+                    <p class = "toolsTextLeft">{project.tools}</p></div>
                 <div class="fiveLeft"></div>
             </div>
         </div>
@@ -46,13 +63,39 @@
     {/if}
 {/each}
 <style>
+    .centered {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: none;
+        width: 90%;
+
+    }
+    .toolsText{
+        color: rgb(156 163 175);
+        margin-top: 2%;
+        font-size: 1.5rem;
+    }
+    .toolsTextLeft{
+        color: rgb(156 163 175);
+        text-align: right;
+        margin-top: 2%;
+        font-size: 1.5rem;
+
+    }
+    .centeredText{
+        font-size: 1rem;
+        color: rgb(156 163 175);
+
+    }
     html{
         overflow: hidden;
     }
     .projDesc {
         background: rgb(0, 0, 0, .44);
         backdrop-filter: blur(11.5px);
-        font-size: .75em;
+        font-size: 1.5rem;
         padding: 5%;
     }
     .goBottom{
@@ -79,6 +122,15 @@
         position: relative;
     }
     .two {
+        grid-column: 3 / 4;
+        grid-row: 1 / 5;
+        aspect-ratio: 2/2;
+        object-fit: cover;
+        filter: saturate(200%);
+        width: 30vw;
+        height: 30vw;
+    }
+    .two2 {
         grid-column: 3 / 4;
         grid-row: 1 / 5;
         aspect-ratio: 2/2;
@@ -141,6 +193,15 @@
         transform: translateX(10vw);
 
     }
+    .twoLeft2 {
+        grid-column: 1 / 4;
+        grid-row: 1 / 5;
+        aspect-ratio: 2/2;
+        object-fit: cover;
+        filter: saturate(200%);
+        width: 30vw;
+        height: 30vw;
+    }
     .twoLeft:hover {
         filter: brightness(100%) saturate(200%);
     }
@@ -152,13 +213,31 @@
 
     }
     @media only screen and (max-device-width: 50rem) {
+        .toolsText {
+            display: none;
+        }
+        .toolsTextLeft {
+            display: none;
+        }
+        .centered {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            display: block;
+        }
         .wrappedBox{
             display: flex;
             justify-content: center;
         }
         .two {
             object-fit: cover;
-            filter: saturate(200%) brightness(50%);
+            width: 100%;
+            height: 100%;
+        }
+        .two2 {
+            object-fit: cover;
+            filter: saturate(200%) brightness(25%);
             width: 100%;
             height: 100%;
         }
@@ -179,7 +258,13 @@
 
         .twoLeft {
             object-fit: cover;
-            filter: saturate(200%) brightness(50%);
+            width: 100%;
+            height: 100%;
+            transform: translateX(0);
+        }
+        .twoLeft2 {
+            object-fit: cover;
+            filter: saturate(200%) brightness(25%);
             width: 100%;
             height: 100%;
             transform: translateX(0);
