5929eea2415569714b119071aa9fa42d7dd6be8d
rpatil5 <rishistar05@gmail.com>
2024-07-13 18:20:26 -0700

    Fixed db access

--- api/hello-world.js
+++ api/hello-world.js
@@ -1,6 +1,11 @@
 import supabase from "../src/lib/db.js";
+import { createClient } from '@supabase/supabase-js'
 
 module.exports = (req, res) => {
+    const supabase = createClient(
+        process.env.VITE_SUPABASE_URL,
+        process.env.VITE_SUPABASE_ANON_KEY
+    )
     let { data, error } = supabase.from('experience').select('*').order('priority', { ascending: true })
     res.send({
         status: 200,
