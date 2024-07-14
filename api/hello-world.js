fc72b40c38d5930624902e5984a8c8fe37274986
rpatil5 <rishistar05@gmail.com>
2024-07-13 18:31:09 -0700

    Changed export

--- api/hello-world.js
+++ api/hello-world.js
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-module.exports = (req, res) => {
+export default async (req, res) => {
     const supabase = createClient(
         process.env.VITE_SUPABASE_URL,
         process.env.VITE_SUPABASE_ANON_KEY
     )
-    let { data, error } = supabase.from('experience').select('*').order('priority', { ascending: true })
+    let { data, error } = await supabase.from('experience').select('*').order('priority', { ascending: true })
     res.send({
         status: 200,
         message: "Hello world!!",
